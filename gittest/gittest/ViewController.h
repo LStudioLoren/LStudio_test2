bc777bc9bfea51229511e37b2e21897ed24f21df
王静龙 <wangjinglong@wangjinglongs-mac-pro.local>
2016-03-26 22:26:25 +0800

    11

---
+++ gittest/gittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest
+//
+//  Created by 王静龙 on 26/3/16.
+//  Copyright © 2016年 王静龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gittest/gittest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gittest
+//
+//  Created by 王静龙 on 26/3/16.
+//  Copyright © 2016年 王静龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
